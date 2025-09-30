a9b3fb2e9cd62a3b3e49300a6334fd140ad83056
kablt--6362 <wogus6362@gmail.com>
2025-09-30 17:19:07 +0900

    State실습-NumberState

--- react/my-react-app/src/App.jsx
+++ react/my-react-app/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import StringState from "./components/State/StringState";
+import NumberState from "./components/State/NumberState";
 
 export default function App() {
   return (
     <div>
       <StringState></StringState>
+      <NumberState></NumberState>
     </div>
   );
 }
