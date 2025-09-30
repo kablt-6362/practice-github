43c14854a17d8435443900373652e4b6e6bbc3e2
kablt--6362 <wogus6362@gmail.com>
2025-09-30 17:28:23 +0900

    State-객체실습

--- react/my-react-app/src/App.jsx
+++ react/my-react-app/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import StringState from "./components/State/StringState";
 import NumberState from "./components/State/NumberState";
+import ObjectState from "./components/State/ObjectState";
 
 export default function App() {
   return (
     <div>
       <StringState></StringState>
       <NumberState></NumberState>
+      <ObjectState></ObjectState>
     </div>
   );
 }
