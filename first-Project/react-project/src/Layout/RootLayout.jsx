50ee60708d78b5c6b8b1df575179f183ed7e64bb
kablt--6362 <wogus6362@gmail.com>
2025-10-17 10:57:03 +0900

    Home.jsx기본html

--- first-Project/react-project/src/Layout/RootLayout.jsx
+++ first-Project/react-project/src/Layout/RootLayout.jsx
@@ -5,12 +5,12 @@ export default function RootLayout() {
   return (
     <div>
       <div className="flex justify-between">
-        <div className="">
+        <div className="flex gap-2">
           <Link to={PATHS.INDEX}>홈으로</Link>
           <Link to={PATHS.MEMOCREAT}>메모 작성</Link>
           <Link to={PATHS.MEMOTITLE}>메모 목록</Link>
         </div>
-        <div className="">
+        <div className="flex gap-2">
           <Link to={PATHS.LOGIN}>로그인</Link>
           <Link to={PATHS.SIGNUP}>회원 가입</Link>
         </div>
--- first-Project/react-project/src/pasges/Home.jsx
+++ first-Project/react-project/src/pasges/Home.jsx
@@ -1,3 +1,23 @@
+import { Link } from "react-router-dom";
+import PATHS from "../Path/paths";
+
 export default function Home() {
-  return <div>Home</div>;
+  const ptag =
+    "자연어로 할 일을 입력하면 AI가 자동으로 구조화된 메모로 변환해주는 지능형메모 관리 서비스입니다.";
+
+  return (
+    <div>
+      <div id="header">
+        <h1>Memo AI</h1>
+        <h2>지능형 메모 관리</h2>
+        <p>{ptag}</p>
+        <div className="flex gap-2 justify-center">
+          <Link className="border-2 " to={PATHS.MEMOCREAT}>메모 작성하기</Link>
+          <Link className="border-2 " to={PATHS.MEMOTITLE}>메모 목록 보기</Link>
+        </div>
+      </div>
+      <div id="body">주요 기능 설명</div>
+      <div id="footer">사용 방법</div>
+    </div>
+  );
 }
