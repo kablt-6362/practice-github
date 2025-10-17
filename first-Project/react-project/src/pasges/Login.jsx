beb82f0c19aa5a15aaad44e2229ecce0ad86f3f1
kablt--6362 <wogus6362@gmail.com>
2025-10-17 11:14:55 +0900

    login,signup컴포넌트 기본작성

--- first-Project/react-project/src/pasges/Login.jsx
+++ first-Project/react-project/src/pasges/Login.jsx
@@ -1,3 +1,20 @@
+import { Link } from "react-router-dom";
+import PATHS from "../Path/paths";
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div>
+      <div>
+        <p>로그인</p>
+        <p>계정에 로그인하세요</p>
+      </div>
+      <div className="flex flex-col  gap-2">
+        <input className="border-2" type="email" />
+        <input className="border-2" type="password" />
+        <input className="border-2" type="submit" value="로그인 하기" />
+        <Link to={PATHS.SIGNUP}>회원가입</Link>
+        <Link to={PATHS.INDEX}>처음으로</Link>
+      </div>
+    </div>
+  );
 }
--- first-Project/react-project/src/pasges/SignUp.jsx
+++ first-Project/react-project/src/pasges/SignUp.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import PATHS from "../Path/paths";
 
-export default function SignUp() {
+export default function SingUp() {
   return (
-    <div>SignUp</div>
-  )
+    <div>
+      <div>
+        <p>회원가입</p>
+        <p>새 계정을 만들어보세요</p>
+      </div>
+      <div className="flex flex-col  gap-2">
+        <input className="border-2" type="email" placeholder="이메일 주소" />
+        <input className="border-2" type="password" placeholder="비밀번호" />
+        <input className="border-2" type="number" placeholder="비밀번호 확인" />
+        <input className="border-2" type="submit" value="회원가입 하기" />
+        <Link to={PATHS.LOGIN}>로그인</Link>
+        <Link to={PATHS.INDEX}>처음으로</Link>
+      </div>
+    </div>
+  );
 }
