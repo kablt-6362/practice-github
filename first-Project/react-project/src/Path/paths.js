c51473ce1de10ed0bc5dadcefd9be4e61b1c3b33
kablt--6362 <wogus6362@gmail.com>
2025-10-21 15:21:13 +0900

    path파일 내용 추가,memolist컴포넌트 추가

--- first-Project/react-project/src/Path/paths.js
+++ first-Project/react-project/src/Path/paths.js
@@ -4,5 +4,9 @@ const PATHS = {
   SIGNUP: "/signup",
   MEMOCREAT: "/memo-creat",
   MEMOTITLE: "/memo-title",
+  MEMO_BASE: "/memo",
+  MEMO_ALL: "/memo/all",
+  MEMO_INCOMPLETE: "/memo/incomplete",
+  MEMO_COMPLETE: "/memo/complete",
 };
 export default PATHS;
